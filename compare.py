3225dce193829a8153123356ec9480e124bce2de
Pavel Chuprikov <pschuprikov@gmail.com>
2014-10-11 03:14:27 +0400

    More informative comparasion.

--- compare.py
+++ compare.py
@@ -4,10 +4,13 @@ import os
 import sys
 import tabulate
 import argparse
+import genblastA_to_gff3
 from collections import namedtuple, defaultdict
 from Bio import SearchIO
+from Bio import SeqIO
 
-AlignmentData = namedtuple('AlignmentData', ['runid', 'score', 'range', 'exons'])
+AlignmentData = namedtuple('AlignmentData', ['runid', 'score', 'matched', 'coverage', 'range', 'exons'])
+
 
 def get_run_id(run_path):
     return os.path.basename(run_path)[:-len('.parseable.out')]
@@ -16,25 +19,56 @@ def get_run_id(run_path):
 def get_tool_id(run_path):
     return get_run_id(run_path).split('.')[-1]
 
+searchio_formats = {'blast': 'blast-xml', 'exonerate': 'exonerate-vulgar'}
 
-def parse_n_fill_run_data(run_path, run_data):
+def parse_n_fill_run_data_searchio(run_path, run_data, querydb):
     run_id = get_run_id(run_path)
-    searchio_format = {'blast': 'blast-xml', 'exonerate': 'exonerate-vulgar'}[get_tool_id(run_path)]
-
-    for query in SearchIO.parse(run_path, searchio_format):
+    run_format = {'blast': 'blast-xml', 'exonerate': 'exonerate-vulgar'}[get_tool_id(run_path)]
+    for query in SearchIO.parse(run_path, run_format):
         for hit in query.hits:
             for hsp in hit.hsps:
                 exons = [x.hit_range for x in hsp.fragments]
+                coverage = 'N/A'
 
-                if get_tool_id(run_path) == 'blast':
-                    score = hsp.bitscore
-                elif get_tool_id(run_path) == 'exonerate':
+                if querydb is not None:
+                    total_matched = sum(x.query_span for x in hsp.fragments)
+                    coverage = '{:.2f}%'.format(100 * total_matched / len(querydb[query.id]))
+
+                if hasattr(hsp, 'score'):
                     score = hsp.score
+                elif hasattr(hsp, 'bitscore'):
+                    score = hsp.bitscore
                 else:
                     score = 'N/A'
 
-                alignment = AlignmentData(run_id, score, hsp.hit_range, exons)
+                if hasattr(hsp, 'ident_num') and hasattr(query, 'seq_len'):
+                    matched = '{:.2f}%'.format(100 * hsp.ident_num / query.seq_len)
+                else:
+                    matched = 'N/A'
+
+                alignment = AlignmentData(run_id, score, matched, coverage, hsp.hit_range, exons)
                 run_data[query.id][hit.id].append(alignment)
+
+
+def parse_n_fill_run_data_genblasta(run_path, run_data):
+    run_id = get_run_id(run_path)
+    with open(run_path, 'r') as runfile:
+        for hit in genblastA_to_gff3.parse_genblastA(runfile):
+                for hsp in hit['hsps'].values():
+                    match = hit['match']
+                    exons = [(hsp['match_start'] - 1, hsp['match_end'] - 1)]
+                    matched = '{:.2f}%'.format(hsp['perc_id'])
+                    alignment = AlignmentData(run_id, match['score'], matched, match['coverage_perc'] + '%',
+                                              (int(match['match_start']), int(match['match_end'])), exons)
+                    run_data[match['query_name']][match['match_name']].append(alignment)
+
+
+def parse_n_fill_run_data(run_path, run_data, querydb):
+    tool_id = get_tool_id(run_path)
+    if tool_id in searchio_formats.keys():
+        parse_n_fill_run_data_searchio(run_path, run_data, querydb)
+    elif tool_id == 'genblasta':
+        parse_n_fill_run_data_genblasta(run_path, run_data)
 
 
 def print_run_data(run_data, output):
@@ -45,13 +79,18 @@ def print_run_data(run_data, output):
             print(tabulate.tabulate(data, headers=AlignmentData._fields), file=output)
             print('\n', file=output)
 
-parser = argparse.ArgumentParser(description='Sweet comparator')
-parser.add_argument('results', nargs='+', help='Results to compare.')
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Sweet comparator')
+    parser.add_argument('results', nargs='+', help='Results to compare.')
+    parser.add_argument('-q', '--query', dest='query')
+    args = parser.parse_args()
 
-all_run_data = defaultdict(lambda: defaultdict(list))
-for path in args.results:
-    parse_n_fill_run_data(path, all_run_data)
-print_run_data(all_run_data, sys.stdout)
+    all_run_data = defaultdict(lambda: defaultdict(list))
 
-
+    querydb = None
+    if args.query is not None:
+        # should we use index or _index_db_?
+        querydb = SeqIO.to_dict(SeqIO.parse(args.query, 'fasta'))
+    for path in args.results:
+        parse_n_fill_run_data(path, all_run_data, querydb)
+    print_run_data(all_run_data, sys.stdout)
--- search.py
+++ search.py
@@ -2,9 +2,12 @@
 
 import argparse
 import os
+from os import path
 import shutil
 import subprocess
 import sweetconfig
+import tempfile
+import genblastA_to_gff3
 
 def dbpath2seq(dbpath):
     return os.path.join(dbpath, 'sequence.fasta')
@@ -15,11 +18,11 @@ class Blast:
 
     @staticmethod
     def writeraw(dbpath, proteinspath, outpath):
-        subprocess.call(['tblastn', '-db', os.path.join(dbpath, os.path.basename(dbpath)), '-query', proteinspath, '-out', outpath])
+        subprocess.check_call(['tblastn', '-db', path.join(dbpath, path.basename(dbpath)), '-query', proteinspath, '-out', outpath])
 
     @staticmethod
     def writeparseable(dbpath, proteinspath, outpath):
-        subprocess.call(['tblastn', '-db', os.path.join(dbpath, os.path.basename(dbpath)), '-query', proteinspath, '-outfmt', '5', '-out', outpath])
+        subprocess.check_call(['tblastn', '-db', path.join(dbpath, path.basename(dbpath)), '-query', proteinspath, '-outfmt', '5', '-out', outpath])
 
 
 class Exonerate:
@@ -29,13 +32,13 @@ class Exonerate:
     @staticmethod
     def writeraw(dbpath, proteinspath, outpath):
         with open(outpath, 'w') as outfile:
-            subprocess.call(['exonerate', '--model', 'protein2genome', '--query',
+            subprocess.check_call(['exonerate', '--model', 'protein2genome', '--query',
                              proteinspath, '--target', dbpath2seq(dbpath)], stdout=outfile)
 
     @staticmethod
     def writeparseable(dbpath, proteinspath, outpath):
         with open(outpath, 'w') as outfile:
-            subprocess.call(['exonerate', '--model', 'protein2genome', '--query', proteinspath,
+            subprocess.check_call(['exonerate', '--model', 'protein2genome', '--query', proteinspath,
                              '--showalignment', 'no', '--target', dbpath2seq(dbpath)], stdout=outfile)
 
 
@@ -46,7 +49,7 @@ class Genewise:
     @staticmethod
     def writeraw(dbpath, proteinspath, outpath):
         with open(outpath, 'w') as outfile:
-            subprocess.call(['genewise', proteinspath, dbpath2seq(dbpath)], stdout=outfile)
+            subprocess.check_call(['genewise', proteinspath, dbpath2seq(dbpath)], stdout=outfile)
 
 
 class Genblasta:
@@ -60,19 +63,28 @@ class Genblasta:
         return newenv
 
     @staticmethod
-    def writeraw(dbpath, proteinspath, outpath):
-        seqpath_abs = os.path.abspath(dbpath2seq(dbpath))
-        outpath_abs = os.path.abspath(outpath)
+    def _prepare_aux_dir(proteinspath, outpath):
+        outpath_abs = path.abspath(outpath)
 
-        auxdirpath = os.path.join(os.path.dirname(outpath_abs), '.aux.' + os.path.basename(outpath_abs))
-        if not os.path.exists(auxdirpath):
+        auxdirpath = path.join(path.dirname(outpath_abs), '.aux.' + path.basename(outpath_abs))
+        if not path.exists(auxdirpath):
             os.mkdir(auxdirpath)
 
-        proteinspath_aux = os.path.abspath(os.path.join(auxdirpath, 'query.fasta'))
+        proteinspath_aux = path.abspath(path.join(auxdirpath, 'query.fasta'))
         shutil.copyfile(proteinspath, proteinspath_aux)
 
-        subprocess.call(['genblasta', '-q', proteinspath_aux, '-t', seqpath_abs, '-o', outpath_abs],
-                        cwd = auxdirpath, env=Genblasta._prepareenv())
+        return auxdirpath, proteinspath_aux
+
+
+    @staticmethod
+    def writeraw(dbpath, proteinspath, outpath):
+        auxdir, proeins_aux = Genblasta._prepare_aux_dir(proteinspath, outpath)
+        subprocess.check_call(['genblasta', '-q', os.path.abspath(proeins_aux), '-t', os.path.abspath(dbpath2seq(dbpath))
+                               , '-o', os.path.abspath(outpath)], cwd=auxdir, env=Genblasta._prepareenv())
+
+    @staticmethod
+    def writeparseable(dbpath, proteinspath, outpath):
+        Genblasta.writeraw(dbpath, proteinspath, outpath)
 
 
 def runraw(args):
@@ -88,21 +100,21 @@ def runparseable(args):
     tools[args.toolname].writeparseable(args.dbname, args.proteins, filename)
     print(filename + ' was written')
 
+if __name__ == '__main__':
+    tools = dict()
+    for x in [Blast(), Genewise(), Genblasta(), Exonerate()]:
+        tools[x.toolname] = x
 
-tools = dict()
-for x in [Blast(), Genewise(), Genblasta(), Exonerate()]:
-    tools[x.toolname] = x
+    parser = argparse.ArgumentParser(description="Sweet bioinformatics tool.")
+    parser.add_argument('operation', choices=['raw', 'parseable'], help='Run mode')
+    parser.add_argument('toolname', choices=tools.keys(), help='Name of a tool to be used')
+    parser.add_argument('proteins', help='Input proteins file name')
+    parser.add_argument('dbname', help='Sequence database name (created with initialize.py)')
+    parser.add_argument('--runname', dest='runname', help='Run name (saving output to runname.%someextension%)')
 
-parser = argparse.ArgumentParser(description="Sweet bioinformatics tool.")
-parser.add_argument('operation', choices=['raw', 'parseable'], help='Run mode')
-parser.add_argument('toolname', choices=tools.keys(), help='Name of a tool to be used')
-parser.add_argument('proteins', help='Input proteins file name')
-parser.add_argument('dbname', help='Sequence database name (created with initialize.py)')
-parser.add_argument('--runname', dest='runname', help='Run name (saving output to runname.%someextension%)')
+    args = parser.parse_args()
 
-args = parser.parse_args()
-
-if args.operation == 'raw':
-    runraw(args)
-elif args.operation == 'parseable':
-    runparseable(args)
+    if args.operation == 'raw':
+        runraw(args)
+    elif args.operation == 'parseable':
+        runparseable(args)
